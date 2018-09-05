181bf254372a332bc9209a3cca233c92f16fe5b6
Liliana Kastilio <lili@snyk.io>
2018-09-05 13:15:51 +0100

    feat: initial commit

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import 'source-map-support/register';
-import { PkgTree, DepType } from './parsers';
-export { buildDepTree, buildDepTreeFromFiles, PkgTree, DepType, };
-declare function buildDepTree(): Promise<PkgTree>;
-declare function buildDepTreeFromFiles(root: string, manifestFilePaths: string[], includeDev?: boolean): Promise<PkgTree>;
